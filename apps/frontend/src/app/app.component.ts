51b5bc8e3d31ec3f6f19a063a80e3e77e73df5bf
n-tien <n-tien@atware.asia>
2022-12-10 15:20:39 +0700

    Rename the site & icon

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mentor-management-system-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private readonly router: Router) {}
+
+  get isNotLogin(): boolean {
+    return !this.router.url.startsWith('/login');
+  }
+}
