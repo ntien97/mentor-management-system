8409ee54731320d315f8d8b90d8947925da453ec
n-tien <n-tien@atware.asia>
2022-12-11 17:13:01 +0700

    Add readme

--- terraform/application/user-data/mss_app.sh
+++ terraform/application/user-data/mss_app.sh
@@ -5,7 +5,7 @@ sudo service docker start
 sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 sudo aws ecr get-login-password --region ap-southeast-1 | sudo docker login --username AWS --password-stdin "${ecr_url}"
-sudo docker run -p 3000:3000 \
+sudo docker run --pull=always -p 3000:3000 \
   --restart always \
   -e POSTGRES_HOST="${postgres_host}" \
   -e POSTGRES_PORT="${postgres_port}" \
